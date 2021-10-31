ecb98f1146874abe3781645e89a15d0d5f36dcc0
MaureenDadap <maureendadap@gmail.com>
2021-11-01 00:54:40 +0800

    fixed ui of login pages

--- customerlogin.php
+++ customerlogin.php
@@ -13,65 +13,54 @@ if (isset($_SESSION['login_user2'])) {
 <html>
 <?= head("Guest Login"); ?>
 
-<body>
+<body class="login">
   <?=
   toTopBtn();
   navbar()
   ?>
   <main>
     <div class="container">
-      <div class="jumbotron">
-        <center>
-          <h1>Hi Guest,<br> Welcome to <span style=" color: gold;" class="edit"> <?= $website_name ?> </span></h1>
-        </center>
-        <br>
-        <p>Kindly LOGIN to continue.</p>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="col-md-5 col-md-offset-4">
-        <label style="margin-left: 5px;color: red;"><span> <?php echo $error;  ?> </span></label>
+      <header class="text-center">
+        <h1>Hi Guest</h1>
+        <h1>Welcome to <span class="website_name"> <?= $website_name ?> </span></h1>
+        <h4>Kindly log in to continue.</h4>
+      </header>
+      <div class="col-md-5 mx-auto mt-5">
         <div class="card">
-          <div style="background-color:rgb(1, 1, 85);" class="panel-heading"> Login </div>
+          <div class="card-header">Login</div>
+
           <div class="card-body">
-
             <form action="" method="POST">
-
-              <div class="row">
+              <div class="row mb-3">
                 <div class="form-group col-xs-12">
-                  <label for="username"><span class="text-danger" style="margin-right: 5px;">*</span> Username: </label>
+                  <label for="username"><span class="text-danger">*</span> Username: </label>
                   <div class="input-group">
+                    <span class="input-group-text bi-person-fill"></span>
                     <input class="form-control" id="username" type="text" name="username" placeholder="Username" required="" autofocus="">
-                    <span class="input-group-btn">
-                      <label style="background-color:rgb(1, 1, 85);" class="btn btn-primary"><span class="bi-person-fill" aria-hidden="true"></label>
-                    </span>
                     </span>
                   </div>
                 </div>
               </div>
 
-              <div class="row">
+              <div class="row mb-3">
                 <div class="form-group col-xs-12">
-                  <label for="password"><span class="text-danger" style="margin-right: 5px;">*</span> Password: </label>
+                  <label for="password"><span class="text-danger">*</span> Password: </label>
                   <div class="input-group">
+                    <span class="input-group-text bi-lock-fill"></span>
                     <input class="form-control" id="password" type="password" name="password" placeholder="Password" required="">
-                    <span class="input-group-btn">
-                      <label style="background-color:rgb(1, 1, 85);" class="btn btn-primary"><span class="bi-lock-fill" aria-hidden="true"></span></label>
-                    </span>
-
                   </div>
                 </div>
               </div>
 
-              <div class="row">
-                <div class="form-group col-xs-4">
-                  <button class="btn btn-primary" name="submit" type="submit" value=" Login ">Submit</button>
-                </div>
+              <label class="text-danger mb-3"><span> <?php echo $error;  ?> </span></label>
 
+              <div class="row mb-3">
+                <div class="form-group col-xs-4">
+                  <button class="btn btn-primary" name="submit" type="submit" value="Login">Submit</button>
+                </div>
               </div>
-              <label style="margin-left: 5px;">or</label> <br>
-              <label style="margin-left: 5px;"><a href="customersignup.php">Create a new account.</a></label>
+              <label>or</label> <br>
+              <label><a href="customersignup.php">Create a new account.</a></label>
 
             </form>
           </div>
--- index.php
+++ index.php
@@ -19,16 +19,6 @@ include("common/components.php");
         <img src="images/bulldog.png" alt="NU LOGO" width="80px" height="80px" style="border-radius:100%;margin-top:80px;">
       </div>
     </div>
-
-    <div class="wide">
-      <div class="col-xs-5 line">
-        <hr>
-      </div>
-      <div class="col-xs-5 line">
-        <hr>
-      </div>
-      <div style="color: rgb(1, 1, 85);" class="tagline">Eat Right, Food is Life.</div>
-    </div>
     <br>
     <div style="height:500px;margin-top:100px ;">
       <center><a class="btn btn-success btn-lg" style="background-color:gold;border:none;color:chocolate;" href="customerlogin.php" role="button"> Order Now </a></center>
